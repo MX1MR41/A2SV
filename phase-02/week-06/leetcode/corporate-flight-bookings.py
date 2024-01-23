292d02593d8a4665bcdb2d59aa688cd0e5c47262
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-01-23 19:57:32 +0300

    Added solution for Corporate Flight Bookings

---
+++ phase-02/week-06/leetcode/corporate-flight-bookings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        res = [0]*(n+1)
+
+        for i, j, k in bookings:
+            res[i-1] += k
+            res[j] -= k
+
+        for i in range(1, n+1):
+            res[i] += res[i-1]
+
+        res.pop()
+        return res
