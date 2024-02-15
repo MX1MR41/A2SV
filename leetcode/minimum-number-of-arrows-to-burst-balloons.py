d16ab51ad10cd2c36bc76eb0483a8d2fa56ec189
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-02-15 10:11:06 +0300

    Add solution for Minimum Number of Arrows to Burst Balloons

--- leetcode/minimum-number-of-arrows-to-burst-balloons.py
+++ leetcode/minimum-number-of-arrows-to-burst-balloons.py
@@ -1,15 +1,15 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort()
-        lastInd = points[0][1]
-        arrows = 1
+        points.sort() # sorting is indispensable
+        arrowPos = points[0][1] # the best position we can shoot the arrow from to hit most targets
+        arrows = 1 # a minimum of 1 arrow is mandatory 
 
-        for i in points[1:]:
-            if i[0] <= lastInd:
-                lastInd = min(lastInd, i[1])
-            else:
-                arrows += 1
-                lastInd = i[1]
+        for i in points[1:]: # traverse from the second element
+            if i[0] <= arrowPos: # if the balloon is within the reach of the arrow that was last shot
+                arrowPos = min(arrowPos, i[1]) # update the arrow position to the max of the intersection of the balloons
+            else: # if the balloon is outside the reach of the the last shot arrow
+                arrows += 1 # we neeed another arrow
+                arrowPos = i[1]  # and the new arrow will be shot at the end of the current balloon
 
 
         return arrows
