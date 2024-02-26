e81040a38fe355e92ba1013fab7abce14dacdf7c
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-02-26 18:47:09 +0300

    Add solution for Merge Two Binary Trees

--- phase-03/week-04/leetcode/merge-two-binary-trees.py
+++ phase-03/week-04/leetcode/merge-two-binary-trees.py
@@ -7,8 +7,14 @@ class Solution:
         if not root2:
             return root1
 
+        # if neither are None, then we create a new node with val
+        # equal to the sum of the two roots' vals
+        # then we recursively do the same for their left and right
+
         merged = TreeNode(root1.val + root2.val)
+
         merged.left = self.mergeTrees(root1.left, root2.left)
+
         merged.right = self.mergeTrees(root1.right, root2.right)
 
         return merged
