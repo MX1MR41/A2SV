a5db44b10e33d374dedea60d745e5ec484e4cd7c
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-03-01 15:17:23 +0300

    Add solution for K-th Symbol in Grammar

---
+++ phase-03/week-04/leetcode/k-th-symbol-in-grammar.py
@@ -0,0 +1,20 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        # this recursive generation of numbers has a pattern 
+        # a given ith row will have its first half of numbers
+        # the same as the (i-1)th row's first half and 
+        # its second half will be the complement of the (i-1)th row's second half
+        
+        def gram(n, k):
+            if n == 1:
+                return 0
+            
+            mid = 2 ** (n - 2)
+
+            if k <= mid:
+                return gram(n - 1, k)
+                
+            else:
+                return 1 - gram(n - 1, k - mid)
+
+        return gram(n, k)
