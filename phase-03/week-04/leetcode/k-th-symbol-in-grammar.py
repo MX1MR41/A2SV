0b6dac51a4c14ee5163c53f185dffbc66e2cbe0b
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-03-01 16:30:10 +0300

    Add solution for K-th Symbol in Grammar

--- phase-03/week-04/leetcode/k-th-symbol-in-grammar.py
+++ phase-03/week-04/leetcode/k-th-symbol-in-grammar.py
@@ -4,17 +4,14 @@ class Solution:
         # a given ith row will have its first half of numbers
         # the same as the (i-1)th row and 
         # its second half will be the complement of the (i-1)th row's 
-        
-        def gram(n, k):
-            if n == 1:
-                return 0
-            
-            mid = 2 ** (n - 2)
+        if n ==1 and k == 1:
+            return 0
+        mid = pow(2, n-1)//2
 
-            if k <= mid:
-                return gram(n - 1, k)
-                
-            else:
-                return 1 - gram(n - 1, k - mid)
+        res = 1
+        if k <= mid:
+            res = self.kthGrammar(n-1, k)
+        else:
+            res = 1 ^ self.kthGrammar(n-1,k-mid)
 
-        return gram(n, k)
+        return res
