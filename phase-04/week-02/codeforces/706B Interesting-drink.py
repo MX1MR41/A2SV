15770c94679c35c7b83233e7a84416a69e619464
Rash <mx1.mr41@gmail.com>
2024-03-18 16:56:23 +0300

    code

--- phase-04/week-02/codeforces/706B Interesting-drink.py
+++
@@ -1,8 +0,0 @@
-from bisect import bisect_right
-n = int(input())
-arr = sorted(list(map(int, input().split())))
-for _ in range(int(input())):
-    d = int(input())
-    ind = bisect_right(arr, d)
-
-    print(ind)
