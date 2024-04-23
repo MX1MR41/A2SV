83574475d294d56fcc4945aea1494b99567803a8
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-04-23 21:13:01 +0300

    Update longest-cycle-in-a-graph.py

--- phase-04/week-06/leetcode/longest-cycle-in-a-graph.py
+++ phase-04/week-06/leetcode/longest-cycle-in-a-graph.py
@@ -1,21 +1,27 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         g = defaultdict(list)
+
         for i in range(len(edges)):
             g[i].append(edges[i])
 
         def dfs(node, visited, path):
-            if node in visited:
-                res = 1
-                i = len(path) - 1
-                while path[i] != node:
-                    i -= 1
-                    res += 1
-                return res
+            # if cycle is found, we iterate backwards in our path list
+            # till we find the starting point of the cycle. At the same time we 
+            # are counting number of nodes in between i.e. the length of that cycle which we return
+            if node in visited: 
+                length = 1
+                ind = len(path) - 1
+                while path[ind] != node:
+                    ind -= 1
+                    length += 1
+                return length
 
             path.append(node)
             visited.add(node)
 
+            # if a cycle hasn't been found yet, we need to explore further
+            # in the adjacency list IF their is an adjacent node
             ans = max(0, dfs(g[node].pop(), visited, path)) if g[node] else 0
 
             return ans
