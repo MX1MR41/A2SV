5458031ca2d5baf02aa633e813a7ecc63a53c4ae
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-05-07 14:36:55 +0300

    Update find-xor-beauty-of-array.py

--- phase-04/week-09/leetcode/find-xor-beauty-of-array.py
+++ phase-04/week-09/leetcode/find-xor-beauty-of-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def xorBeauty(self, nums: List[int]) -> int:
         # XOR magic
+        # simplifying the expanded equation will yield just the XOR of the elements
         ans = 0
         for i in nums:
             ans ^= i
