c499d49aa135ddf353f87bb73a4107cad8812436
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-05-12 21:44:34 +0300

    Create best-time-to-buy-and-sell-stock-with-cooldown.py

---
+++ phase-04/week-09/leetcode/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # modified code from 
+        # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+        # added a third dimension which stores the cooldown profit
+        # i.e. if we sold a stock the previous day, we cannot buy today
+        # so the cooldown value for today is the same as the stock-less(sell) value of today-1
+        # and among the ways we can be stock-less today is also the cooldown of yesterday
+        n = len(prices)
+        if n <= 1: return 0
+        dp = [[0]*3 for _ in range(n)]
+
+        dp[0][0] = 0
+        dp[0][1] = -prices[0] 
+        dp[0][2] = 0
+
+        dp[1][0] = max(0, dp[0][1] + prices[1])
+        dp[1][1] = max(dp[0][1], - prices[1]) 
+        dp[1][2] = 0
+
+        for day in range(1, n):
+            
+            dp[day][0] = max(dp[day-1][0], dp[day-1][1] + prices[day], dp[day-1][2])
+            # the stock-less profit of yesterday
+            # selling the stock-full of yesterday 
+            # the stock-less cooldown of yesterday
+
+            dp[day][1] = max(dp[day-1][1], dp[day-2][0] - prices[day])
+            # the stock-full of yesteday
+            # buying today's stock by the profit of before yesterday's stock-less, 
+            # cuz we can't buy today if we sold tomorrow
+
+            dp[day][2] = dp[day-1][0]
+            # the cooldown profit of today is the stock-less profit of yesterday
+            # i.e. not buying a stock today after selling(becoming stock-less) yesterday
+
+
+        return dp[-1][0]
