d8e2dfc3eb1697ca6d48de5bebb0f5f2c37c84d4
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-05-18 20:37:17 +0300

    Update partition-to-k-equal-sum-subsets.py

--- phase-04/week-10/leetcode/partition-to-k-equal-sum-subsets.py
+++ phase-04/week-10/leetcode/partition-to-k-equal-sum-subsets.py
@@ -1,41 +1,39 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        mask = 0
-        unmask = 0
+        # backtarking + dp + bit manipulation
+        # we try various combinations of the numbers and keep track of
+        # the used numbers using a bitmask
+        mask = unmask = 0
         for i in range(len(nums)):
             unmask |= 1 << i
+            
         dp = defaultdict(bool)
-        target = sum(nums)/k
-        if target % int(target): return False
+
+        target = sum(nums) / k # the sum of each subset 
+        
+        if target % int(target): return False # invalid partitioning
 
         def dfs(mask, part, tot):
 
-            if mask in dp: return dp[mask]
-            if part > k or tot > target: return False
-            if part == k and mask == unmask and tot == target:
+            if mask in dp: # done and memoized already some subtree back left
+                return dp[mask]
+            if part > k or tot > target: # invalid partition
+                return False
+            if part == k and mask == unmask and tot == target: # validated
                 dp[mask] = True
                 return True
 
-            if tot == target: 
+            if tot == target: # a new subset needs to be formed
                 part += 1
                 tot = 0
 
             for i in range(len(nums)):
-                if not mask & (1 << i):
+                if not mask & (1 << i): # nums[i] hasn't been used down this subtree
                     temp = dfs(mask | (1 << i), part, tot + nums[i])
-                    if temp:
+                    if temp: # break immediately, no need to search anymore
                         dp[mask] = True
                         return True
 
             dp[mask] = False
 
         return dfs(mask, 1, 0)
-
-
-
-
-
-            
-                
-
-        
