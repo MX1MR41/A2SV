2d8f9391f8fe878ee1889ad0ae8c2cef5cc5621e
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-06-07 10:11:38 +0300

    Update replace-words.py

--- phase-04/week-12/leetcode/replace-words.py
+++ phase-04/week-12/leetcode/replace-words.py
@@ -3,6 +3,7 @@ class TrieNode:
         self.root = [None] * 26
         self.end = False
 
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
@@ -22,25 +23,24 @@ class Trie:
     def search(self, word):
         dummy = self.root
         ind = ord(word[0]) - 97
-        if dummy.root[ind] is None: return ""
+        if dummy.root[ind] is None:
+            return ""
         root_word = ""
 
         for i in word:
             ind = ord(i) - 97
-            if dummy.end: return root_word
-            if dummy.root[ind] is None: 
-                if not any([i for i in dummy.root]):
-                    return root_word
-                else: return ""
 
-            dummy = dummy.root[ind]
+            if dummy.root[ind] is None:
+                return ""
+
+            dummy = dummy.root[ind]            
 
             root_word += i
+            
+            if dummy.end:
+                return root_word
 
         return root_word if dummy.end else ""
-
-
-
 
 
 class Solution:
@@ -50,13 +50,11 @@ class Solution:
         for word in dictionary:
             prefix.insert(word)
 
-
         for i in range(len(sentence)):
             word = sentence[i]
             root = prefix.search(word)
-            if not root: continue
+            if not root:
+                continue
             sentence[i] = root
 
         return " ".join(sentence)
-
-        
