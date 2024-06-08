0b7a1e5ca07816793bdaaa516bd7997ed615fc8d
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-06-08 10:00:05 +0300

    Update largest-number.py

--- phase-02/week-02/largest-number.py
+++ phase-02/week-02/largest-number.py
@@ -1,42 +1,13 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        ans, d = "", defaultdict(list) 
-        # dictionary to hold str form of nums each mapped to the leftmost digit
+        nums = [str(num) for num in nums]
 
-        for i in range(len(nums)):
-            d[str(nums[i])[0]].append(str(nums[i]))
+        def custom_sort(x):
+            return x * 10
 
-        for j in d:
-            l = d[j]
-            #bubble sort to get the combination of nums that give the largest integer
-            for i in range(len(l)-1):
-                for j in range(i, len(l)):
-                    s1 = "".join(l) # without swapping
-                    l[i], l[j]= l[j], l[i]
-                    s2 = "".join(l) # after swapping
-                    if s2 < s1:
-                        l[i], l[j]= l[j], l[i]
+        nums.sort(key=custom_sort, reverse=True)
 
-        dkeys = sorted(d.keys(), reverse = True)
+        if nums[0] == "0":
+            return "0"
 
-        for i in dkeys:
-            ans += "".join(d[i])
-
-        #handling edgecase of repeating zeros like "00"
-        zeros, found = "", False
-        for i in range(len(ans) - 1): 
-#1st condition handles "100, 200", then check for consecutive zeros with no other dig in front
-            if ans[0] == '0' and ans[i] == '0' and ans[i+1] == '0':
-                found = True
-                j = i+1
-                while j < len(ans)-1 and ans[j] == "0":
-                    j += 1
-                zeros = '0' * (j-i+1)
-                break
-
-        if found:
-            ans = ans.replace(zeros, "0")
-
-        return ans
-            
-        
+        return "".join(nums)
