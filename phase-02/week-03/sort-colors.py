5aa68dbd5249d618eeba763454b392d28e356d76
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-06-12 13:35:01 +0300

    Update sort-colors.py

--- phase-02/week-03/sort-colors.py
+++ phase-02/week-03/sort-colors.py
@@ -24,4 +24,24 @@ class Solution:
             l -= 1
 
 
+
+# a better solution
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        l, r = 0, len(nums) - 1
+        i = 0
+
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            else:
+                i += 1
+
+
         
