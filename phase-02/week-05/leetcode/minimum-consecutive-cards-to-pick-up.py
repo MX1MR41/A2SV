3422640430304108c52f2e0d86c006a509b8dd1d
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-07-15 12:37:02 +0300

    Update minimum-consecutive-cards-to-pick-up.py

--- phase-02/week-05/leetcode/minimum-consecutive-cards-to-pick-up.py
+++ phase-02/week-05/leetcode/minimum-consecutive-cards-to-pick-up.py
@@ -1,16 +1,17 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
-        res = float("inf")
-        d = {}
-        n = len(cards)
-        for i in range(n):
-            a = cards[i]
-            if a in d:
-                ind = d[a]
-                res = min(res, i-ind + 1)
-
-            d[a] = i
-
-        return res if res != float("inf") else -1
-
+        left = 0
+        min_pickup = float("inf")
+        card_count = defaultdict(int)
         
+        for right in range(len(cards)):
+            card = cards[right]
+            card_count[card] += 1
+
+            
+            while card_count[card] > 1:
+                min_pickup = min(min_pickup, right - left + 1)
+                card_count[cards[left]] -= 1
+                left += 1
+        
+        return min_pickup if min_pickup != float("inf") else -1
