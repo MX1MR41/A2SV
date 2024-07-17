843a530e45db4a1fda53c53e4b2e2fcd68947b34
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-07-17 13:44:06 +0300

    Update delete-nodes-and-return-forest.py

--- phase-04/week-19/leetcode/delete-nodes-and-return-forest.py
+++ phase-04/week-19/leetcode/delete-nodes-and-return-forest.py
@@ -5,9 +5,10 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def delNodes(
-        self, root: Optional[TreeNode], to_delete: List[int]
-    ) -> List[TreeNode]:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        # do dfs on the tree and if the current node you are visiting needs to be deleted,
+        # you do not return it back to its parent to be re-attached (you detach it)
+
         ans = []
         delete = set(to_delete)
 
@@ -15,11 +16,16 @@ class Solution:
             nonlocal ans
             if node:
                 if node.val in delete:
-                    left = trav(node.left)
+                    left = trav(node.left) # none if either empty or to node.left needs to be deleted
                     right = trav(node.right)
+                    # if node.left wasn't deleted, we need to add it as its own tree
+                    # since its parent was deleted
+                    if left: 
+                        ans.append(left)
+                    if right:
+                        ans.append(right)
 
-                    if left: ans.append(left)
-                    if right: ans.append(right)
+                    # returns None by defualt indicating deletion
 
                 else:
                     node.left = trav(node.left)
@@ -28,6 +34,9 @@ class Solution:
                     return node
 
         root = trav(root)
-        if root: ans.append(root)
+        if root: # in case the root itself wasn't deleted, we need to count it too
+            ans.append(root)
 
         return ans
+
+        
