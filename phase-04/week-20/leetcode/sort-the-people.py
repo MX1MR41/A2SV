303048f767f97a98564bc75c98c9b97e8c1f72bb
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-07-22 13:41:15 +0300

    Update sort-the-people.py

--- phase-04/week-20/leetcode/sort-the-people.py
+++ phase-04/week-20/leetcode/sort-the-people.py
@@ -1,5 +1,3 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        zipped = list(zip(names, heights))
-        zipped.sort(key = lambda x: x[1], reverse = True)
-        return [z[0] for z in zipped]
+        return [z[0] for z in sorted(list(zip(names, heights)), key = lambda x: x[1], reverse = True )]
