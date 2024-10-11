c838ebdd9352a02e9ba5e9a59fa4c28152692c12
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-10-11 14:41:31 +0300

    Create cheapest-flights-within-k-stops.py

---
+++ phase-06/week-01/leetcode/cheapest-flights-within-k-stops.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+
+        dp = [float("inf")] * n
+        dp[src] = 0
+
+        for _ in range(k + 1):
+
+            temp_dp = dp[:]
+
+            for u, v, p in flights:
+                if dp[u] != float("inf"):  # only if u has been reached before in a previous iteration
+                    temp_dp[v] = min(temp_dp[v], dp[u] + p)
+
+            dp = temp_dp
+
+        return dp[dst] if dp[dst] != float("inf") else -1
