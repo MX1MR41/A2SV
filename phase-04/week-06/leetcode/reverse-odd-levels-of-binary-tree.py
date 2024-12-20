cdbfbe3c61f42cb7f053496b79dbc441ee47dcbd
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-12-20 20:47:30 +0300

    Update reverse-odd-levels-of-binary-tree.py

--- phase-04/week-06/leetcode/reverse-odd-levels-of-binary-tree.py
+++ phase-04/week-06/leetcode/reverse-odd-levels-of-binary-tree.py
@@ -4,27 +4,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def bfs(que):
-            lev = 0
+        # store all nodes by their respective levels and respective order in a dictionary
+        # for every level and next level, do a criss-cross stitching
 
-            while que:
-                if lev % 2:
-                    temp = list(que)
-                    vals = [node.val for node in temp]
-                    for node in temp:
-                        node.val = vals.pop()
+        # storing nodes 
+        nodes = defaultdict(list)
+        lev = 0
+        que = deque([root])
+        while que:
+            for _ in range(len(que)):
+                node = que.popleft()
+                if node.left:
+                    que.append(node.left)
+                    que.append(node.right)
 
-                for _ in range(len(que)):
-                    node = que.popleft()
-                    if node.left:
-                        que.append(node.left)
-                        que.append(node.right)
+                nodes[lev].append(node)
 
-                lev += 1
+            lev += 1
 
-        bfs(deque([root]))
+        # travel every level
+        for i in range(0, lev - 1):
+            curr_level = deque(nodes[i])
+            nxt_level = deque(nodes[i + 1])
+
+            for _ in range(len(curr_level)):
+                node = curr_level.popleft()
+
+                # if it is an even level, we just have to stitch nodes from the lower level
+                # in a directly reverse manner
+                if not i % 2:
+                    node.left = nxt_level.pop()
+                    node.right = nxt_level.pop()
+
+                # if it is odd, since this level has been reversed, the nodes from lower level
+                # are stitched in a reverse but cross-child manner
+                else:
+                    node.right = nxt_level.pop()
+                    node.left = nxt_level.pop()
 
         return root
