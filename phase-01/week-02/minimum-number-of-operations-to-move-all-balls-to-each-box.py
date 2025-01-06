d807cd7c894f1fe8e55aa04bcbb7b94a74b5ca2d
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-01-06 17:33:21 +0300

    Update minimum-number-of-operations-to-move-all-balls-to-each-box.py

--- phase-01/week-02/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ phase-01/week-02/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,15 +1,35 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        res = [0] * len(boxes)
+        # prefix sum + suffix sum
+        # let our indices be [a,b,c,d,e] and let us assume they are all "1", let us solve for c
+        # from the left side => abs(a - c) + abs(b - c) => c - a + c - b => 2*c - (a + b)
+        # for index i from the left the total will be i * (no of 1 indices) - (sum of indices)
+        # and from the right it will be ther reverse i.e -(i*(no) - (sum))
+        n = len(boxes)
+        prefix = []
+        pre_cnt = pre = 0
+        for i in range(n):
+            prefix.append((pre_cnt, pre))
+            if boxes[i] == "1":
+                pre_cnt += 1
+                pre += i
 
-        for i in range(len(boxes)):
-            moves = 0
+        suffix = []
+        suff_cnt = suff = 0
 
-            for j in range(len(boxes)):
-                if boxes[j] == "1":
-                    moves += abs(i - j)
+        for i in range(n - 1, -1, -1):
+            suffix.append((suff_cnt, suff))
+            if boxes[i] == "1":
+                suff_cnt += 1
+                suff += i
 
-            res[i] = moves
+        suffix.reverse()
 
-        return res
-        
+        ans = [0] * n
+        for i in range(n):
+            pre_cnt, pre = prefix[i]
+            suff_cnt, suff = suffix[i]
+
+            ans[i] = (i * pre_cnt) - pre - (i * suff_cnt) + suff
+
+        return ans
