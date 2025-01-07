8765a7cb0b3758ddbd0b7d046a7190482056b4b3
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-01-07 12:17:06 +0300

    Create string-matching-in-an-array.py'

---
+++ phase-06/week-14/leetcode/string-matching-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ans = []
+        n = len(words)
+        for i in range(n):
+            for j in range(n):
+                if  i == j:
+                    continue
+
+                if words[i] in  words[j]:
+                    ans.append(words[i])
+                    break
+
+
+        return ans
