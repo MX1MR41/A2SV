284a5b2835d2ac44cee939e9dcf1de2105135b7e
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-01-09 19:49:46 +0300

    Create construct-quad-tree.py

---
+++ phase-06/week-14/leetcode/construct-quad-tree.py
@@ -0,0 +1,42 @@
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        # dfs
+        # keep dividing the grid into four quadrants until you have only one cell which is base case
+        # build up from there
+        def quad(row, col, dist):
+            if dist == 0:
+                node = Node(grid[row][col], True, None, None, None, None)
+                return node
+
+            dist //= 2
+
+            topLeft = quad(row, col, dist)
+            topRight = quad(row, col + dist, dist)
+            bottomLeft = quad(row + dist, col, dist)
+            bottomRight = quad(row + dist, col + dist, dist)
+
+            allLeaves = topLeft.isLeaf and topRight.isLeaf and bottomLeft.isLeaf and bottomRight.isLeaf
+
+            allSameValues = topLeft.val == topRight.val == bottomLeft.val == bottomRight.val
+
+
+            if allLeaves and allSameValues:
+                node = Node(topLeft.val, True, None, None, None, None)
+            else:
+                node = Node(0, False, topLeft, topRight, bottomLeft, bottomRight)
+
+            return node
+
+        return quad(0, 0, len(grid))
