70a00bf5a0d4407a53b0a00e2a7e6466cec70087
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-01-18 17:48:12 +0300

    Update minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py

--- phase-06/week-15/leetcode/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ phase-06/week-15/leetcode/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -1,3 +1,4 @@
+# BFS SOLUTION
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
         # bfs + queue + dp
@@ -45,3 +46,57 @@ class Solution:
                             que.append((cost + 1, (ni, nj)))
 
         return visited[(m - 1, n - 1)]
+
+
+# HEAP SOLUTION
+
+class Solution:
+    def minCost(self, grid: List[List[int]]) -> int:
+        # heap + dp
+        # Create a visited hashmap that will store the minimum cost it took
+        # to get to visited[(row, col)].
+        # Traverse the grid starting from (0, 0) in a bfs manner
+        # for every (i, j), try the immediate neighbor based on the sign with cost + 0
+        # then try the other possible neighbors of different signs with cost + 1.
+        # Expand a cell (i, j) only if the cost it took to reach it is less than
+        # its last minimum cost, i.e. visited[(i, j)].
+        # Store next options in a heap to get the least cost immediately
+
+
+        dirs = {1: (0, 1), 2: (0, -1), 3: (1, 0), 4: (-1, 0)}
+        m, n = len(grid), len(grid[0])
+
+        inbound = lambda i, j: 0 <= i < m and 0 <= j < n
+
+        visited = defaultdict(lambda: float("inf"))
+
+        heap = [(0, (0, 0))]
+
+        while heap:
+            
+            cost, (i, j) = heappop(heap)
+            if (i, j) == (m - 1, n - 1):
+                return cost
+
+            if cost >= visited[(i, j)]:
+                continue
+
+            visited[(i, j)] = min(visited[(i, j)], cost)
+
+            sign = grid[i][j]
+            di, dj = dirs[sign]
+            ni, nj = i + di, j + dj
+
+            if inbound(ni, nj) and cost < visited[(ni, nj)]:
+                heappush(heap, (cost, (ni, nj)))
+
+            for d in range(1, 5):
+                if d != sign:
+                    di, dj = dirs[d]
+                    ni, nj = i + di, j + dj
+
+                    if inbound(ni, nj) and cost + 1 < visited[(ni, nj)]:
+                        heappush(heap, (cost + 1, (ni, nj)))
+
+        return visited[(m - 1, n - 1)]
+
