28de6b04580048c23250af531cff26272bbc50be
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-02-26 11:10:37 +0300

    Update maximum-absolute-sum-of-any-subarray.py

--- phase-06/week-21/leetcode/maximum-absolute-sum-of-any-subarray.py
+++ phase-06/week-21/leetcode/maximum-absolute-sum-of-any-subarray.py
@@ -1,21 +1,19 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
         # prefix sum with kadane's approach
-        # two passes: 1 to get the maximum positive sum, the other to get minimum neg sum
+        # keep track of the maximum positive sum and the minimum negative sum seen so far
+        # use kadane's approach on the positive prefix sum and a modified kadane on neg sum
         res = 0
-        pre = 0
+        pos = 0
+        neg = 0
         for i in nums:
-            pre += i
-            res = max(res, abs(pre))
-            if pre < 0:
-                pre = 0
-
-        pre = 0
-        for i in nums:
-            pre += i
-            res = max(res, abs(pre))
-            if pre > 0:
-                pre = 0
+            pos += i
+            neg += i
+            res = max(res, abs(pos), abs(neg))
+            if pos < 0:
+                pos = 0
+            if neg > 0:
+                neg = 0
 
         return res
         
