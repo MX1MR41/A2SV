dc092a5ac487beef45e3b2cff41faacfb706472f
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-03-15 12:13:09 +0300

    Update D_a_Good_String.py

--- phase-06/week-23/codeforces/D_a_Good_String.py
+++ phase-06/week-23/codeforces/D_a_Good_String.py
@@ -4,57 +4,48 @@ https://codeforces.com/contest/1385/problem/D
 
 """
 
-import sys, threading
 
-input = lambda: sys.stdin.readline().strip()
+def dfs(l, r, x, s):
 
-def main():
-    def dfs(l, r, x, s):
+    if l == r:
+        return 0 if s[l] == x else 1
 
-        if l == r:
-            return 0 if s[l] == x else 1
-        
-        mid = (l + r)//2 + 1
-        
-
-        left = mid - l - s[l:mid].count(x)
-        first = left + dfs(mid, r, chr(ord(x) + 1), s)
-
-        right = r - mid + 1 - s[mid:r + 1].count(x)
-        second = right + dfs(l, mid - 1, chr(ord(x) + 1), s)
-
-        return min(first, second)
-
-    for _ in range(int(input())):
-        n = int(input())
-        s = input()
-
-        if n == 1:
-            if s == 'a':
-                print(0)
-            else:
-                print(1)
-
-            continue
-
-        mid = n//2
-        
-        left = mid - s[:mid].count('a')
-        
-        
-        first = left + dfs(mid, n - 1, chr(ord('a') + 1), s)
-
-        right = n - mid - s[mid:n].count('a')
-        second = right + dfs(0, mid - 1, chr(ord('a') + 1), s)
-
-        print(min(first, second))
+    mid = (l + r)//2 + 1
 
 
-if __name__ == '__main__':
-    sys.setrecursionlimit(1 << 30)
-    threading.stack_size(1 << 27)
+    left = mid - l - s[l:mid].count(x)
+    first = left + dfs(mid, r, chr(ord(x) + 1), s)
 
-    main_thread = threading.Thread(target=main)
-    main_thread.start()
-    main_thread.join()
+    right = r - mid + 1 - s[mid:r + 1].count(x)
+    second = right + dfs(l, mid - 1, chr(ord(x) + 1), s)
+
+    return min(first, second)
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+
+    if n == 1:
+        if s == 'a':
+            print(0)
+        else:
+            print(1)
+
+        continue
+
+    mid = n//2
+
+    left = mid - s[:mid].count('a')
+
+
+    first = left + dfs(mid, n - 1, chr(ord('a') + 1), s)
+
+    right = n - mid - s[mid:n].count('a')
+    second = right + dfs(0, mid - 1, chr(ord('a') + 1), s)
+
+    print(min(first, second))
+
+
+
+
 
