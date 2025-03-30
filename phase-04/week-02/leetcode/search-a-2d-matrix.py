83007698d4e70ffd23ee277d4aec152acd00e1f5
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-03-30 11:58:22 +0300

    Update search-a-2d-matrix.py

--- phase-04/week-02/leetcode/search-a-2d-matrix.py
+++ phase-04/week-02/leetcode/search-a-2d-matrix.py
@@ -1,29 +1,30 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def search(arr, x):
-            l, r = 0, len(arr) - 1
-            while l <= r:
-                mid = (l + r)//2
-                curr = arr[mid]
-                if curr == x: return True
-                elif curr < x: l = mid + 1
-                else: r = mid - 1
-            return False
+        # math + binary search
+        # order the cells from 0 to m*n - 1 going from left to right and top to bottom
+        # do binary seacrh to get the target
+
+        def getkthcell(k, m, n):
+            row = k//n
+            col = k - (row)*n
+            return matrix[row][col]
+
+        m, n = len(matrix), len(matrix[0])
+
+        left, right = 0, m*n - 1
 
 
-        lo, hi = 0, len(matrix) - 1
+        while left <= right:
+            mid = (left + right)//2
+            cell = getkthcell(mid, m, n)
+            if cell == target:
+                return True
 
-        while lo <= hi:
-            mid = (lo + hi)//2
-            curr = matrix[mid]
-            if curr:
-                if curr[0] <= target <= curr[-1]:
-                    return search(curr, target)
-                elif target < curr[0]:
-                    hi = mid - 1
-                else:
-                    lo = mid + 1
+            if cell < target:
+                left = mid + 1
+            else:
+                right = mid - 1
 
         return False
-
+            
         
