eded930cf9bd13a558f1368da22d2ab27447d279
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-04-01 11:41:27 +0300

    Update solving-questions-with-brainpower.py

--- phase-04/week-10/leetcode/solving-questions-with-brainpower.py
+++ phase-04/week-10/leetcode/solving-questions-with-brainpower.py
@@ -1,39 +1,26 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        # solution uses two dp arrays dp and dp2
-        # dp[i] will store the processed result of questions[i]
-        # dp2[i] will store the maximum result seen so far from i to the end of the array
-        # when processing result for dp[i], the answer will be the maximum of solving i'th question
-        # plus solving the most available next question OR solving i'th question plus solving ANY other
-        # available question upto the end of the array. For that we need to know the maximum available
-        # score from all available questions; hence dp2.
+        # dp
+        # start from the last index, and for every index i with points p and skip s,
+        # you have the ability to earn p and any maximum p from indices starting from i + s + 1,
+        # or just skip it and get the immediately next index's previous computed score
+        # choose whichever is maximum
+
         n = len(questions)
-        if n <= 2: return max([i[0] for i in questions])
 
-        dp = dp2 = [0] * (n)
+        dp = [0] * n
+        dp[-1] = questions[-1][0]
 
-        dp[-1] = dp2[-1] = questions[-1][0]
-
-        dp[-2] = questions[-2][0]
-
-        dp2[-2] = max(dp2[-1], dp[-2])
-
-
-
-        for i in reversed(range(n-2)):
-            curr, skip = questions[i]
+        for i in range(n - 2, -1, -1):
+            points, skip = questions[i]
             nxt = i + skip + 1
+
             if nxt < n:
-                if nxt < n - 1:
-                    dp[i] = max(curr + dp[nxt], curr + dp2[nxt+1])
-                else:
-                    dp[i] = curr + dp[nxt]
-            else:
-                dp[i] = curr
+                points += dp[nxt]
 
-            dp2[i] = max(dp[i], dp2[i+1])
+            dp[i] = max(dp[i + 1], points)
 
-            
 
         return max(dp)
-        
+
+
