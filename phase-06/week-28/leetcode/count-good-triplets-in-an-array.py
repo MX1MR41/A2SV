22795f734d429147adba76e117016e689c2f5fea
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-04-15 22:52:19 +0300

    Update count-good-triplets-in-an-array.py

--- phase-06/week-28/leetcode/count-good-triplets-in-an-array.py
+++ phase-06/week-28/leetcode/count-good-triplets-in-an-array.py
@@ -1,3 +1,5 @@
+# Solution 1 
+
 class SegmentTree:
     def __init__(self, n):
         self.n = 2 ** ceil(log2(n))
@@ -81,3 +83,88 @@ class Solution:
             res += prev[i] * nxt[i]
 
         return res
+
+
+
+
+# Solution 2
+
+class Solution:
+    def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        # merge sort
+        # for every num x, count the number of elements that come before it in both
+        # arrays and store in prev dictionary, count the number of elements that come
+        # after it in both arrays in nxt dictionary, then compute the good triplets
+        # as prev[x] * nxt[x] by considering x as the middle element
+        # use merge sort algorithm to compare and count in parallel
+
+        def merge(left, right, inds, prev, nxt):
+            
+            # counting process
+            # since we know that left and right are already independently sorted arrays
+            # we can use that property to quickly calculate how many numbers in right 
+            # occur after num for every num in left, AND how many number in left occur
+            # before num for every num in right. The indices themselves tell us how
+            # many steps we've moved and consequently how many nums have been counted
+
+            i = j = 0
+
+            while j < len(right):
+                # try to find the last number in left that occurs before right[j]
+                # pointer i will stops just past that last valid number
+                while i < len(left) and inds[left[i]] < inds[right[j]]:
+                    # since left[i] occurs before right[j], that means it also occurs
+                    # before all nums in right starting from j upto the end of right
+                    nxt[left[i]] += len(right) - j
+
+                    i += 1
+
+                # i will stop at the first number in left that occurs after right[j],
+                # hence i numbers occur before right[j]
+                prev[right[j]] += i
+                j += 1
+
+
+
+            # merging process
+            merged = []
+            i = j = 0
+            while i < len(left) and j < len(right):
+                if inds[left[i]] < inds[right[j]]:
+                    merged.append(left[i])
+                    i += 1
+                else:
+                    merged.append(right[j])
+                    j += 1
+
+            merged.extend(left[i:])
+            merged.extend(right[j:])
+
+            return merged
+
+        def mergeSort(arr, inds, prev, nxt):
+            if len(arr) <= 1:
+                return arr
+
+            mid = len(arr) // 2
+
+            left = mergeSort(arr[:mid], inds, prev, nxt)
+            right = mergeSort(arr[mid:], inds, prev, nxt)
+
+            return merge(left, right, inds, prev, nxt)
+
+        inds1 = {num: ind for ind, num in enumerate(nums1)}
+        prev = defaultdict(int)
+        nxt = defaultdict(int)
+
+        mergeSort(nums2, inds1, prev, nxt)
+
+        res = 0
+
+        n = len(nums1)
+
+        for i in range(n):
+            res += prev[i] * nxt[i]
+
+        return res
+
