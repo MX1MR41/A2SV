bb8cf1dd64e95977691e9f2da57ed7fab5efcbe3
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-05-02 15:36:32 +0300

    Update push-dominoes.py

--- phase-06/week-30/leetcode/push-dominoes.py
+++ phase-06/week-30/leetcode/push-dominoes.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution:
     def pushDominoes(self, dominoes: str) -> str:
         # bfs
@@ -66,3 +67,66 @@ class Solution:
                 final += res[i]
 
         return final
+
+
+# Solution 2
+
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        # dp
+        # use two arrays, for right and left, to keep track of how long it will take
+        # for a dominoe to fall to either the right or left due to by being pushed
+        # either initially or by other dominoes
+
+        n = len(dominoes)
+        right = [-1] * n # -1 if this dominoe doesn't fall to the right
+        last_right = -1 # most recent dominoe from the left that falls to the right
+        for i in range(n):
+            if dominoes[i] == "R":
+                last_right = i # this node becomes the most recent dominoe to fall right
+                right[i] = 0 # this node falls at time 0
+                
+            elif dominoes[i] == "L":
+                last_right = -1
+            else:
+                if last_right != -1:
+                    right[i] = i - last_right
+
+
+        left = [-1] * n # -1 if this dominoe doesn't fall to the left
+        last_left = -1 # most recent dominoe from the right that falls to the left
+        for i in range(n - 1, -1, -1):
+            if dominoes[i] == "L":
+                last_left = i
+                left[i] = 0
+            elif dominoes[i] == "R":
+                last_left = -1
+            else:
+                if last_left != -1:
+                    left[i] = last_left - i
+
+
+
+        res = ""
+
+        for i in range(n):
+            if right[i] == left[i]:
+                res += "."
+
+            elif right[i] != -1 and left[i] != -1:
+                if right[i] < left[i]:
+                    res += "R"
+                else:
+                    res += "L"
+
+            elif right[i] == -1:
+                res += "L"
+
+            elif left[i] == -1:
+                res += "R"
+                
+            else:
+                res += "."
+
+        return res
+
