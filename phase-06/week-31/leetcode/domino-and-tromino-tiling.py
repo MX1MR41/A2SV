12d382a60c05425f20d0d03083763e3845932521
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-05-06 12:04:39 +0300

    Update domino-and-tromino-tiling.py

--- phase-06/week-31/leetcode/domino-and-tromino-tiling.py
+++ phase-06/week-31/leetcode/domino-and-tromino-tiling.py
@@ -1,31 +1,32 @@
 class Solution:
     def numTilings(self, n: int) -> int:
+        # dp + prefix sum
+        # to fill the board upto n, we could either add 1 vertical domino from n-1;
+        # or add two horizonatl dominoes to n - 2; or for every n <= n -3, we could find a way
+        # to fit a combination of two trominoes with multiple dominoes, and we could mirror that
+        # unique combination so there would be two ways.
+        # So to fill a board of size n, we can take dp[n - 1] or dp[n - 2] or 2 * (sum(dp[0...n-3]))
+        # we could store the sum in a prefix sum for ease
         MOD = 10**9 + 7
 
-        if n == 0:
-            return 1 
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
+        if n <= 2:
+            return n
 
         dp = [0] * (n + 1)
 
-        # Base cases
+        
         dp[0] = 1
         dp[1] = 1
         dp[2] = 2
 
-        # Calculate dp[k] using the O(k) sum
-        for k in range(3, n + 1):
-            # Contribution from ending with V (m=1)
-            dp[k] = dp[k - 1]
-            # Contribution from ending with HH (m=2)
-            dp[k] = (dp[k] + dp[k - 2]) % MOD
+        prefixdp = dp[:]
+        prefixdp[1] = 2
+        prefixdp[2] = 4
 
-            # Contribution from ending with irreducible blocks of size m >= 3
-            # Summation: 2 * dp[k-3] + 2 * dp[k-4] + ... + 2 * dp[0]
-            for j in range(k - 3, -1, -1): # Loop from j=k-3 down to 0
-                dp[k] = (dp[k] + (2 * dp[j]) % MOD) % MOD
+        
+        for k in range(3, n + 1):
+            dp[k] = (dp[k - 1] + dp[k - 2] + 2 * prefixdp[k - 3]) % MOD
+
+            prefixdp[k] = (prefixdp[k - 1] + dp[k]) % MOD
 
         return dp[n]
