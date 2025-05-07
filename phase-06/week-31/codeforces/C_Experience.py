6eda695085db1680bceebf691c24efd9280e402a
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-05-07 13:15:52 +0300

    Update C_Experience.py

--- phase-06/week-31/codeforces/C_Experience.py
+++ phase-06/week-31/codeforces/C_Experience.py
@@ -2,82 +2,89 @@
 
 from collections import defaultdict
 
-
 class UnionFind:
-    def __init__(self, n_max_players):
-
-        self.root = {i: i for i in range(1, n_max_players + 1)}
+    def __init__(self, n):
+        self.root = {i: i for i in range(1, n + 1)}
         self.rank = defaultdict(int)
-        self.xp = defaultdict(int)
-        self.offset = defaultdict(int)
+        self.xp = defaultdict(int) # total xp of each player 
+        self.offset = defaultdict(int) # offset to keep track of the undeserved xp's a player gets
 
-    def find(self, i):
-        if self.root[i] == i:
-            return i
+    def find(self, x):
+        # we move up the tree to the root while accumulating the xp's and offsets of all nodes
+        # except the root node. After path compression, all nodes will point directly to the root
+        # so to get the total xp of a player we will add the root's xp into the players then deduct the player's offset.
 
-        original_parent = self.root[i]
-        true_root = self.find(original_parent)
+        if self.root[x] == x:
+            # we return 0 for both xp and offset so that we don't accumulate a root's xp
+            # and offset to a child
+            return x, 0, 0
 
-        self.offset[i] += self.offset[original_parent]
+        rootx, totxp, netoffset = self.find(self.root[x])
 
-        self.root[i] = true_root
-        return true_root
+        # accumulate and compress path
+        self.xp[x] += totxp
+        self.offset[x] += netoffset
+        self.root[x] = rootx
+
+        return rootx, self.xp[x], self.offset[x]
 
     def union(self, x, y):
-        rootx = self.find(x)
-        rooty = self.find(y)
+        rootx, _, _ = self.find(x)
+        rooty, _, _ = self.find(y)
 
         if rootx == rooty:
             return
 
-        if self.rank[rootx] < self.rank[rooty]:
+        rankx, ranky = self.rank[rootx], self.rank[rooty]
+        if rankx > ranky:
+            self.root[rooty] = rootx
+            # add the larger tree's xp as an offset to the smaller tree because we don't
+            # want the smaller tree to get undeserved xp. 
+            # We don't need to accumulate the larger tree's offset tho, cuz we're attaching it to a root
+            # and a root's offset is zero, until it gets merged into another tree
+            self.offset[rooty] += self.xp[rootx]
 
+        elif rankx < ranky:
             self.root[rootx] = rooty
+            self.offset[rootx] += self.xp[rooty]
 
-            self.offset[rootx] = self.xp[rootx] - self.xp[rooty]
-        elif self.rank[rootx] > self.rank[rooty]:
-
-            self.root[rooty] = rootx
-            self.offset[rooty] = self.xp[rooty] - self.xp[rootx]
         else:
-
-            self.root[rooty] = rootx
-            self.offset[rooty] = self.xp[rooty] - self.xp[rootx]
-            self.rank[rootx] += 1
+            self.root[rootx] = rooty
+            self.offset[rootx] += self.xp[rooty]
+            self.rank[rooty] += 1
 
     def getxp(self, x):
-        root_of_x = self.find(x)
 
-        return self.xp[root_of_x] + self.offset[x]
+        rootx, _, _ = self.find(x)
 
-    def addxp(self, x, value_v):
-        root_of_x = self.find(x)
+        if rootx != x:
+            # since the path will be compressed, the node will point directly to the root 
+            # and it will have accumulated the xps and offsets of all its ancestors except the root's
+            # so to compute x's xp, we add the root's xp to x's xp and we deduct x's offset.
+            # we don't really need to deduct the root's offset from the answer because if a node is a root,
+            # its offset will always be zero, until it gets merged into another tree
+            return self.xp[rootx] + self.xp[x] - self.offset[x]
+        
+        else:
+            return self.xp[x] - self.offset[x]
 
-        self.xp[root_of_x] += value_v
+
+    def addxp(self, x, xp):
+        rootx, _, _ = self.find(x)
+        self.xp[rootx] += xp
 
 
+n, q = list(map(int, input().split()))
 
-n_players, m_queries = map(int, input().split())
+dsu = UnionFind(n)
+for _ in range(q):
+    query = list(input().split())
+    if query[0] == "add":
+        dsu.addxp(int(query[1]), int(query[2]))
 
-dsu = UnionFind(n_players)
+    elif query[0] == "join":
+        dsu.union(int(query[1]), int(query[2]))
 
-results_to_print = []
-
-for _ in range(m_queries):
-    query_parts = input().split()
-    command = query_parts[0]
-
-    if command == "add":
-        player_x_id = int(query_parts[1])
-        value_v = int(query_parts[2])
-        dsu.addxp(player_x_id, value_v)
-    elif command == "join":
-        player_x_id = int(query_parts[1])
-        player_y_id = int(query_parts[2])
-        dsu.union(player_x_id, player_y_id)
     else:
-        player_x_id = int(query_parts[1])
-        exp = dsu.getxp(player_x_id)
-        results_to_print.append(str(exp))
-
-print("\n".join(results_to_print) + "\n")
+        xp = dsu.getxp(int(query[1]))
+        print(xp)
