39c4e44846a726d333af91e91955acffaaea6560
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-05-14 21:02:22 +0300

    Update subsets.py

--- phase-06/week-32/leetcode/subsets.py
+++ phase-06/week-32/leetcode/subsets.py
@@ -1,37 +1,29 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         # bit manipulation
-        # we initlaize a bitmask with bits set for every num in nums
-        # i.e. if nums = [2, 3, 5] the bitmask will have the 2th, 3th and 5th bit set
-        # bitmask |= (1 << 2), bitmask |= (1 << 3), bitmask |= (1 << 5), bitmask = 101100
-        # then to find all the subsets, we can keep subtracting 1 from our bitmask, and this 
-        # will force the bitmask to iterate over every possible combination of bits 
-        # from bitmask (101100) upto 000000. And to ensure we don't process a combination with
-        # foreign nums (nums that we didn't originally have) like 000111, we AND it with our
-        # original bitmask so that only the original nums remain, 000111 & 101100 = 000100.
+        # use a bitmask of length n( where n is len(nums)) and all bits set to 1
+        # if n == 3, bitmask = 111, where 1 indicates that nums[i] should be included in the 
+        # current subset that is represented by the current bitmask. Then, to go over all possible
+        # subsets, we keep subtracting 1 from the bitmask until we eventually reach 0. 
+        # 111 (7) -> 110 (6) -> 101 (5) -> 100 (4) -> 011 (3) -> 010 (2) -> 001 (1) -> 000 (0)
+        # for every iteration of the bitmask, we build out subset based on whether an index's
+        # bit is set to 1 or not
 
-        mask = 0
+        n = len(nums)
 
-        for i in nums:
-            i += 10
-            mask |= 1 << i
+        mask = (1 << n) - 1
 
-        init = mask
+        subsets = [[]]
 
-        res = []
         while mask:
-            curr = []
-            for i in range(32):
+            subset = []
+            for i in range(n):
                 if mask & (1 << i):
-                    curr.append(i - 10)
+                    subset.append(nums[i])
 
-            res.append(curr)
+            subsets.append(subset)
 
             mask -= 1
-            mask = mask & init
 
-
-        res.append([])
-
-        return res
-
+        return subsets
+        
