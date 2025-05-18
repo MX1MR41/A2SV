7cb4e24fb1dabcbc13027783128bdc4d4986420b
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-05-18 15:50:35 +0300

    Update painting-a-grid-with-three-different-colors.py

--- phase-06/week-32/leetcode/painting-a-grid-with-three-different-colors.py
+++ phase-06/week-32/leetcode/painting-a-grid-with-three-different-colors.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution:
     def colorTheGrid(self, m: int, n: int) -> int:
         # dp
@@ -88,6 +89,135 @@ class Solution:
         return res 
 
 
+
+
+# Solution 2
+
+class Solution:
+    def multiply(self, matA, matB):
+        """
+        Given two matrices matA and matB where num of cols of matA == num of rows in matB,
+        this function performs matrix multiplication matA x matB and returns the result
+        """
+
+        rows = len(matA)
+        cols = len(matB[0])
+
+        res = [[0 for _ in range(cols)] for _ in range(rows)]
+
+        for r in range(rows):
+            row = matA[r]
+            for c in range(cols):
+                col = [matB[i][c] for i in range(cols)]
+
+                cell = 0
+                for i in range(cols):
+                    cell = (cell + row[i] * col[i]) % self.MOD
+
+                res[r][c] = cell
+
+        return res
+
+    def power(self, mat, exponent):
+        """
+        Given a matrix mat and an integer exponent, this function computes the result of raising
+        mat to the power of exponent. It does this in a binary exponentiation method
+        """
+
+        if exponent == 1:
+            return mat
+
+        half = self.power(mat, exponent // 2)
+
+        res = self.multiply(half, half)
+
+        if exponent % 2 == 1:
+            res = self.multiply(mat, res)
+
+        return res
+
+
+    def colorTheGrid(self, m: int, n: int) -> int:
+        # dp + matrix exponentiation
+        # since we are going to transform a vector multiple times using the same rules,
+        # we can use matrix exponentiation. The transformation rules will be represented in a matrix
+        # where matrix[i][j] == 1 if patterns[j] can be put side by side with patterns[i]
+        # then, we raise the matrix to the power of n - 1 (the number of columns except the first)
+        # because we apply the transformation that number of times. We use binary exponentiation for
+        # logarithmic exponentiation.
+
+
+        self.MOD = 10**9 + 7
+
+        patterns = set()
+
+        def can_be_side_by_side(pat1, pat2):
+            for i in range(m):
+                if pat1[i] == pat2[i]:
+                    return False
+
+            return True
+
+        # function to backtrack and enumerate all possible patterns that a single column can have
+        def dfs(pat, i):
+            nonlocal patterns
+
+            if i == m:
+                patterns.add(pat)
+                return 
+
+            list_pat = list(pat)
+            prev = list_pat[-1]
+
+            for color in [1, 2, 3]:
+
+                if color != prev:
+                    new_list_pat = list_pat + [color]
+                    new_pat = tuple(new_list_pat)
+
+                    dfs(new_pat, i + 1)
+
+
+        # backtrack with the first cell colored as red(1), blue(2) and green(3)
+        dfs((1,), 1)
+        dfs((2,), 1)
+        dfs((3,), 1)
+
+
+        patterns = [list(pat) for pat in patterns]
+        len_patterns = len(patterns)
+
+        # matrix[i][j] = True is patterns[j] can be next to patterns[i]
+        matrix = [[0 for _ in range(len_patterns)] for _ in range(len_patterns)]
+
+        for i in range(len_patterns):
+            pat1 = patterns[i]
+            for j in range(len_patterns):
+                if i == j:
+                    continue
+
+                pat2 = patterns[j]
+
+                if can_be_side_by_side(pat1, pat2):
+                    matrix[i][j] = True
+
+
+        if n == 1:
+            return len_patterns % self.MOD
+
+
+        powered_matrix = self.power(matrix, n - 1)
+
+        initial = [[1 for _ in range(len_patterns)]]
+
+        res = self.multiply(initial, powered_matrix)
+
+        tot = 0
+        for i in res[0]:
+            tot = (tot + i) % self.MOD
+
+        return tot
+        
             
 
             
