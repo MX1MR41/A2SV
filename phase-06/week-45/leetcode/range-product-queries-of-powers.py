78792dc6466e9edb6be6a86dbb1b88d70739f607
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-08-12 11:26:46 +0300

    Update range-product-queries-of-powers.py

--- phase-06/week-45/leetcode/range-product-queries-of-powers.py
+++ phase-06/week-45/leetcode/range-product-queries-of-powers.py
@@ -1,21 +1,13 @@
 class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         # prefix product
+        # any number can be represented as powers of two
+        # the powers can be computed from the binary representation of the number
+        # wherever a bit i is set, then 2^(i) is a power that makes up n
         powers = []
-        need_one = True
-        while n > 0 and log2(n) > 0:
-            p = int(log2(n))
-            if log2(n) == p:
-                need_one = False
-
-            powers.append(2**p)
-
-            n -= 2**p
-
-        if need_one:
-            powers.append(1)
-
-        powers.reverse()
+        for i in range(32):
+            if n & (1 << i):
+                powers.append(2**i)
 
         for i in range(1, len(powers)):
             powers[i] *= powers[i - 1]
