c599e3a3d59cc6b0b092ea0a4bacf5372d2ce318
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2025-09-03 19:42:17 +0300

    Update find-the-number-of-ways-to-place-people-i.py

--- phase-06/week-48/leetcode/find-the-number-of-ways-to-place-people-i.py
+++ phase-06/week-48/leetcode/find-the-number-of-ways-to-place-people-i.py
@@ -1,25 +1,30 @@
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
+        # sorting
+        # once the points are sorted in increasing order of x and decreasing order of y,
+        #  points[i] and points[i + k] can be valid if no point[i + 1 ... i + k - 1]
+        # is located inside or on them
 
-        points.sort(key=lambda x: [-x[0], x[1]])
+        points.sort(key=lambda x: [x[0], -x[1]])
 
         res = 0
 
         for i in range(len(points)):
-            r1, c1 = points[i]
+            x1, y1 = points[i]
             for j in range(i + 1, len(points)):
                 can = True
 
-                r2, c2 = points[j]
-                if r2 > r1 or c2 < c1:
+                x2, y2 = points[j]
+                if x2 < x1 or y2 > y1:
                     continue
                 for k in range(i + 1, j):
-                    r3, c3 = points[k]
-                    if r1 <= r3 <= r2 or c1 <= c3 <= c2:
+                    x3, y3 = points[k]
+                    if x1 <= x3 <= x2 and y1 >= y3 >= y2:
                         can = False
                         break
 
                 if can:
+
                     res += 1
 
         return res
