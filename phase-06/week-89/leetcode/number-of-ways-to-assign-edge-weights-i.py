c5b129c7e497a1f9c65457f08359134facbd9014
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2026-06-15 14:04:05 +0300

    Update number-of-ways-to-assign-edge-weights-i.py

--- phase-06/week-89/leetcode/number-of-ways-to-assign-edge-weights-i.py
+++ phase-06/week-89/leetcode/number-of-ways-to-assign-edge-weights-i.py
@@ -2,6 +2,11 @@ class Solution:
     def assignEdgeWeights(self, edges: List[List[int]]) -> int:
         # bfs + dp
         # counting dp
+        # if there are x ways to get odd sum for n edges, and y ways to get even sum for n edges,
+        # then to get odd for n + 1 edges we have two options: one is to add 1 to each even way of n,
+        # hence now we have x ways to get odd, another option is to add 2 to each odd way of n,
+        # hence now we have additional y ways to get odd for n + 1, so in total x + y ways,
+        
         MOD = 10**9 + 7
         g = defaultdict(list)
         for u, v in edges:
