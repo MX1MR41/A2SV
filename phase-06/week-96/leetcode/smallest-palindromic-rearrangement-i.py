9807ff2062de3e6f6e171693a9eeb3214144a90d
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2026-07-28 09:06:36 +0300

    Create smallest-palindromic-rearrangement-i.py

---
+++ phase-06/week-96/leetcode/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,34 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        # heao
+        cnt = Counter(s)
+        multi = []
+        single = []
+        for x, f in cnt.items():
+            if f > 1:
+                heappush(multi, (x, f))
+            else:
+                heappush(single, x)
+
+
+        left = right = ""
+
+        while multi:
+            x, f = heappop(multi)
+
+            left += x
+            right += x
+
+            f -= 2
+
+            if f > 1:
+                heappush(multi, (x, f))
+            elif f == 1:
+                heappush(single, x)
+
+        while single:
+            left += heappop(single)
+
+        return left + right[::-1]
+
+        
